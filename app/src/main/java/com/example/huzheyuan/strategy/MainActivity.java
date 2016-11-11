e8985dcad10a29c150a7c80cfbbd407fce1ab5a9
Leo428 <huzheyuan428@gmail.com>
2016-11-10 18:58:30 -0800

    import file 10%

--- app/src/main/java/com/example/huzheyuan/strategy/MainActivity.java
+++ app/src/main/java/com/example/huzheyuan/strategy/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.huzheyuan.strategy;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,6 +13,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -22,7 +24,6 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -79,9 +80,10 @@ public class MainActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
-
         if (id == R.id.nav_camera) {
             // Handle the camera action
+            Toast.makeText(MainActivity.this,"Import",Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(MainActivity.this,importActivity.class));
         } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_slideshow) {
---
+++ app/src/main/java/com/example/huzheyuan/strategy/importActivity.java
@@ -0,0 +1,13 @@
+package com.example.huzheyuan.strategy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class importActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_import);
+    }
+}
